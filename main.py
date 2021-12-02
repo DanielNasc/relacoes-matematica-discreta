b5915369ff754c7c078eaafd57897d57831fc458
DanielNasc <danielnasc15987@gmail.com>
2021-12-02 15:12:59 -0300

    checar se R é relação

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from re import sub
+from sys import exit
 from simple_term_menu import TerminalMenu
 from checar_transitividade import checar_transitividade
 from checar_reflexividade import reflexiva_S_em_S, reflexiva_S_em_T
@@ -12,6 +13,10 @@ def main():
     S = get_conjunto('S')
     T = get_conjunto('T') if opcao == 1 else None
     R = tratar_relacao()
+
+    if not (checar_se_R_e_relacao(R, S, T) if opcao == 1 else checar_se_R_e_relacao(R, S, S)):
+        print("\nA relação R não é uma relação de {}".format('S em T' if opcao == 1 else 'S em S'))
+        exit(1)
 
     if opcao == 0:
         print_result(R, 'reflexiva', reflexiva_S_em_S(S, R), 'reflexivo')
@@ -35,6 +40,7 @@ def tratar_relacao():
     for elemento in entrada.split('),'):
         # Separar os elementos do par
         par = tuple([x.replace(')', '') for x in elemento.split(',')])
+
         # Adicionar o par na relação R se ele não estiver repetido
         if not par in R:
             R.append(par)
@@ -45,6 +51,14 @@ def get_conjunto(c):
     conjuntos = sub('[^0-9a-zA-Z,]', '', conjuntos)
     conjuntos = conjuntos.split(',')
     return conjuntos
+
+def checar_se_R_e_relacao(R, S, T):
+
+    for par in R:
+        if not par[0] in S or not par[1] in T:
+            return False
+
+    return True
 
 if __name__ == '__main__':
     main()
